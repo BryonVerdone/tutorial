e095407a799adb501177d42d7865705759ccfee8
BryonVerdone <bryonverdone@gmail.com>
2022-04-17 13:04:11 -0400

    simple list

--- src/index.js
+++ src/index.js
@@ -3,16 +3,26 @@ import ReactDOM from 'react-dom';
 //CSS
 import './index.css';
 // setup vars
-const firstBook = {
-  img: 'https://images-na.ssl-images-amazon.com/images/I/8144Vic9C5L._AC_UL604_SR604,400_.jpg',
-  title: 'I Love You to the Moon and Back',
-  author: 'Amelia Hepworth',
-};
-const secondBook = {
-  img: 'https://images-na.ssl-images-amazon.com/images/I/81a5KHEkwQL._AC_UL302_SR302,200_.jpg',
-  title: `Oh, the Places You'll Go!  `,
-  author: 'Dr. Seuss',
-};
+
+const books = [
+  {
+    img: 'https://images-na.ssl-images-amazon.com/images/I/8144Vic9C5L._AC_UL604_SR604,400_.jpg',
+    title: 'I Love You to the Moon and Back',
+    author: 'Amelia Hepworth',
+  },
+  {
+    img: 'https://images-na.ssl-images-amazon.com/images/I/81a5KHEkwQL._AC_UL302_SR302,200_.jpg',
+    title: `Oh, the Places You'll Go!  `,
+    author: 'Dr. Seuss',
+  },
+  {
+    img: 'https://images-na.ssl-images-amazon.com/images/I/71aG+xDKSYL._AC_UL604_SR604,400_.jpg',
+    title: `The 48 Laws of Power `,
+    author: 'Robert Greene',
+    desc: 'blah blah blah',
+  },
+];
+console.log(books);
 // const author = ;
 // const title = ;
 // const img =
@@ -20,35 +30,23 @@ const secondBook = {
 function BookList() {
   return (
     <section className='booklist'>
-      {/* <Book desc='some random text' />
-      <Book price='$3.99' store='tjmax' /> */}
-      <Book
-        img={firstBook.img}
-        title={firstBook.title}
-        author={firstBook.author}
-      />
-      <Book
-        img={secondBook.img}
-        title={secondBook.title}
-        author={secondBook.author}
-      />
+      {books.map((book) => {
+        // const { img, title, author } = book;
+        return <Book book={book}></Book>;
+      })}
     </section>
   );
 }
 const Book = (props) => {
   console.log(props);
 
+  const { img, title, author, desc } = props.book;
   return (
     <article className='book'>
-      {/* <img style={{ width: 300 }} src={img} alt='' />
-      <h1>{title.toUpperCase()}</h1>
+      <img style={{ width: 300 }} src={img} alt='' />
+      <h1>{title}</h1>
       <h4>{author}</h4>
-      <span>{props.desc}</span>
-      <span>{props.price}</span>
-      <span>{props.store}</span> */}
-      <img style={{ width: 300 }} src={props.img} alt='' />
-      <h1>{props.title}</h1>
-      <h4>{props.author}</h4>
+      <span>{desc}</span>
     </article>
   );
 };
